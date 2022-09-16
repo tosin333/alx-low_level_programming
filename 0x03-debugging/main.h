5a9252c1d26930d7e446bc7e3283c26a60237d8f
tosin <akinsemoyintosin1gmail.com>
2022-09-16 08:32:23 -0700

    my devops projrct

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+
+void positive_or_negative(int);
+int largest_number(int, int, int);
+void print_remaning_days(int, int, int);
+int convert_day(int, int);
+
+#endif
+
